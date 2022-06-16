cdd007ef544c32358853d65ac2c2af3a0a646f0f
sam <sam_pridmore@hotmail.co.uk>
2022-06-16 12:49:28 +0100

    added a delete function

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,64 @@
 import './App.scss';
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 import NavBar from './components/NavBar/NavBar';
 import Main from './containers/Main/Main';
 
-
-
 function App() {
+  const [inputValue, setInputValue] = useState('');
+  const [usersToDo, setUsersToDo] = useState([]);
+  const [checked, setChecked] = useState();
 
-const [inputValue, setInputValue] = useState('')
-const [usersToDo, setUsersToDo] = useState([])
-const [checked, setChecked] = useState()
+  const usersToDoArr = [];
 
-const usersToDoArr = []
+  const handleInput = (event) => {
+    setInputValue(event.target.value);
+  };
 
-
-const handleInput = (event) => {
-  setInputValue(event.target.value)
-}
-
-const handleReset = (event) => {
-  setUsersToDo([])
-}
-
-const resetInput = (event) => {
-  // setCurrentInputVal(event.nativeEvent.path[1][0].value) 
-}
-
-const handleSubmit = (event) => {
-  event.preventDefault();
-  let currentInput = inputValue
-  if (inputValue === '') {
-    alert('Please Enter A ToDo')
-  }
-  usersToDoArr.push(currentInput)
-  setUsersToDo(usersToDoArr => [...usersToDoArr, currentInput])
-}
-
-const handleCheckBox = (event) => {
-  let checked = event.target.checked;
-  setChecked(checked)
-
-  if (checked) {
-
-  }
-}
+  const handleReset = (event) => {
+    setUsersToDo([]);
+  };
 
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    let currentInput = inputValue;
+    if (inputValue === '') {
+      return alert('Please Enter A ToDo');
+    } else {
+      usersToDoArr.push(currentInput);
+      setUsersToDo((usersToDoArr) => [...usersToDoArr, currentInput]);
+    }
+    event.target[0].value = ''
+  };
 
+  const handleCheckBox = (event) => {
+    let checked = event.target.checked;
+    setChecked(checked);
 
-const handleDelete = (event) => {
-  console.log(event);
-  const foundElement = event.target.parentNode.__reactFiber$2vqh0osxwot.alternate.key;
-  const copyUserToDo = usersToDo
-  copyUserToDo.splice(foundElement, 1)
-  console.log(copyUserToDo);
-  setUsersToDo()
-}
+    if (checked) {
+    }
+  };
 
+  const handleDelete = (event) => {
+    const foundElement = event.target.parentElement.parentElement
+    const foundElementValue = event.target.parentElement.parentElement.children[0].children[1].innerHTML;
+    const copyUserToDo = usersToDo;
+
+     copyUserToDo.splice(foundElementValue, 1);
+    setUsersToDo(copyUserToDo);
+    foundElement.remove()
+  };
   return (
     <div className="App">
-      <NavBar handleReset={handleReset}/>
-      <Main handleCheckBox={handleCheckBox} resetInput={resetInput} handleDelete={handleDelete} usersToDo={usersToDo} handleSubmit={handleSubmit} handleInput={handleInput}/>
+      <NavBar handleReset={handleReset} />
+      <Main
+        handleCheckBox={handleCheckBox}
+        handleDelete={handleDelete}
+        usersToDo={usersToDo}
+        handleSubmit={handleSubmit}
+        handleInput={handleInput}
+      />
     </div>
   );
 }
--- src/containers/Main/Main.jsx
+++ src/containers/Main/Main.jsx
@@ -6,14 +6,14 @@ import Buttons from '../../components/Buttons/Buttons';
 
 const Main = (props) => {
 
-  const {handleCheckBox, handleInput, handleSubmit, usersToDo,handleDelete, resetInput, } = props
+  const {handleCheckBox, handleInput, handleSubmit, usersToDo,handleDelete } = props
 
 
   return (
     <div className="main">
       <form onSubmit={handleSubmit} className='main__form'>
         <Inputs name={'userInput'} placeHolder={'Add your task here...'} handleInput={handleInput} />
-        <Buttons onClick={resetInput} buttonText={'Add'}/>
+        <Buttons buttonText={'Add'}/>
       </form>
       <UserToDo handleCheckBox={handleCheckBox} handleDelete={handleDelete} usersToDo={usersToDo} />
     </div>
